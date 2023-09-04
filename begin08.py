8d19a9ca1af5bf617062fc432c6497876d2fc4f8
isroilovazizbek <azizbekisroilov009@gmail.com>
2023-09-04 04:43:50 -0600

    8-ishlandi

--- begin08.py
+++ begin08.py
@@ -1,7 +1,8 @@
 # Assign 5 (int value) to 'a' variable;
-
+a=5
 # Assign 4 (int value) to 'b' variable;
-
+b=4
 # Assign 8 (int value) to 'c' variable;
-
-# Print the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin08';
+c=8
+# Print the result of that expression '(a*b/c)
+print(a*b/c)
